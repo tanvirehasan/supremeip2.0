45068a2ba2b16cd0220dbf2acf50b969b07e0319
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-01 03:38:53 +0600

    Create client.php

---
+++ client.php
@@ -0,0 +1,128 @@
+<?php
+include_once "inc/db.php";
+include_once "inc/function.php";
+include_once "inc/header.php";
+?>
+
+<!-- main content -->
+<main class="col-md-10 ms-md-auto col-lg-10 mt-5 mt-md-0 p-2" style="overflow: hidden;">
+         <div class="row p-0 mx-0 m-0">
+            <?php
+            $a = 1;
+            while ($a <= 60) { ?>
+                <div class="col-4 col-md-2 p-1 m-0">
+                    <div class="card p-2 bg-light" style="border: 1px solid red;">
+                        <img src="assets/mediacenter/client/logo.jpeg" alt="" style="width: 100%;">
+                    </div>
+
+                </div>
+            <?php $a++;
+            } ?>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <h3 class="section_title_page "> <i class="fa fa-circle-o-notch" aria-hidden="true"></i> Our Clients</h3>
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading37">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse37" aria-expanded="false" aria-controls="flush-collapse37" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Who can file a trademark in Bangladesh?</button>
+                        </h2>
+                        <div id="flush-collapse37" class="accordion-collapse collapse" aria-labelledby="flush-heading37" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p>
+                                    <font color="#424242">An application for registration of a trademark in Bangladesh may be made by any person claiming to be the proprietor/owner of a mark for those goods and services for which he is using or intends to use the mark. At the time of filing the application, the proprietor must have the intention to use the mark.</font>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading38">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse38" aria-expanded="false" aria-controls="flush-collapse38" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Symbols of Trademarks</button>
+                        </h2>
+                        <div id="flush-collapse38" class="accordion-collapse collapse" aria-labelledby="flush-heading38" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">Trademarks in Bangladesh may be designated by the following symbols:</span></p>
+                                <p>™ (for an unregistered trademark, but for which application has been filed)<br></p>
+                                <p>℠ (for an unregistered service mark, but for which application has been filed)<br></p>
+                                <p>® (for a registered trademark)<br></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading39">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse39" aria-expanded="false" aria-controls="flush-collapse39" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">What is a trademark?</button>
+                        </h2>
+                        <div id="flush-collapse39" class="accordion-collapse collapse" aria-labelledby="flush-heading39" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">A trademark, trade mark, or trade-mark is a distinctive sign or indicator used by an individual, business organization, or other legal entity to help consumers identify that its products or services with which the trademark appears originate from a unique source, and to help distinguish its products or services from those of other entities.</span></p>
+                                <p>A trademark is typically a name, word, phrase, logo, symbol, design, image, or a combination of these elements. There is also a range of non-conventional trademarks comprising marks which do not fall into these standard categories, such as those based on color, smell, or sound.<br></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading40">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse40" aria-expanded="false" aria-controls="flush-collapse40" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Duration of Trademark registration</button>
+                        </h2>
+                        <div id="flush-collapse40" class="accordion-collapse collapse" aria-labelledby="flush-heading40" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">Once a trademark is registered in Bangladesh, it is valid for a period of 7 years from the date of application. The trademark registration can then be renewed for a further period of 10 years from the date of expiration of registration or of the last renewal of the registration.</span><br></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading41">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse41" aria-expanded="false" aria-controls="flush-collapse41" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Well Known Trademarks Bangladesh</button>
+                        </h2>
+                        <div id="flush-collapse41" class="accordion-collapse collapse" aria-labelledby="flush-heading41" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">The Trademarks Registry recognizes well-known trademarks in Bangladesh on the basis of their international, national and cross border reputation.</span></p>
+                                <p>The Trademark Act, 2009 provides protection to well-known trademarks at two levels:<br></p>
+                                <p>Action against registration of similar marks<br></p>
+                                <p><span style="font-size: 14.304px;">Action against misuse of well-known trademarks</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading42">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse42" aria-expanded="false" aria-controls="flush-collapse42" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">What is a Service mark in Bangladesh?</button>
+                        </h2>
+                        <div id="flush-collapse42" class="accordion-collapse collapse" aria-labelledby="flush-heading42" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">A service mark is the same as a trademark except that it identifies and distinguishes the source of a service rather than a product.</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading43">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse43" aria-expanded="false" aria-controls="flush-collapse43" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Is it possible to have a descriptive trademark registered in Bangladesh?</button>
+                        </h2>
+                        <div id="flush-collapse43" class="accordion-collapse collapse" aria-labelledby="flush-heading43" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">No, it is not possible to register such a trademark.</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header p-0" id="flush-heading44">
+                            <button class="accordion-button collapsed fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse44" aria-expanded="false" aria-controls="flush-collapse44" style="font-family:Raleway !important; color:#027ab6 !important; text-transform:none !important;">Can Sound and Smell be registered as a trademark in Bangladesh?</button>
+                        </h2>
+                        <div id="flush-collapse44" class="accordion-collapse collapse" aria-labelledby="flush-heading44" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body fw-normal fs-6" style="font-family:Anek Bangla !important; color:#7a7a7a !important; text-transform:none !important;">
+                                <p><span style="font-size: 14.304px;">No, Sound and Smell marks cannot be registered as a trademark in Bangladesh.</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
+<?php include_once 'inc/footer.php' ?>
